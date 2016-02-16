b477dde5235f3244f6a9cddf608dbecfda426625
jojotjebaby <joris.hart14@gmail.com>
2016-02-17 00:02:06 +0100

    #1
    The website for Hedonis. Completely coded in Symfony 3

---
+++ src/SiteBundle/Controller/HomeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="site_index")
+     */
+    public function indexAction()
+    {
+    	//get the last 5 articles.
+        return $this->render('SiteBundle:Home:index.html.twig');
+    }
+    /**
+     * @Route("/about", name="site_about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('SiteBundle:Home:about.html.twig');
+    }
+   	/**
+     * @Route("/ouwenduiker", name="site_beer_ouwenduiker")
+     */
+    public function oldDuikerAction()
+    {
+        return $this->render('SiteBundle:Beer:duiker.html.twig');
+    }
+    /**
+     * @Route("/excusemewhileikissmystout", name="site_excuse")
+     */
+    public function stoutAction()
+    {
+        return $this->render('SiteBundle:Beer:stout.html.twig');
+    }
+    /**
+     * @Route("/news", name="site_news")
+     */
+    public function newsAction()
+    {
+        return $this->render('SiteBundle:Home:news.html.twig');
+    }
+    /**
+     * @Route("/verkoop", name="site_sales")
+     */
+    public function salesAction()
+    {
+        return $this->render('SiteBundle:Home:sales.html.twig');
+    }
+    /**
+     * @Route("/contact", name="site_contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('SiteBundle:Home:contact.html.twig');
+    }
+
+
+}
---
+++ src/SiteBundle/Entity/Picture.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Picture
+ *
+ * @ORM\Table(name="picture")
+ * @ORM\Entity(repositoryClass="SiteBundle\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="src", type="string", length=255)
+     */
+    private $src;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alt", type="string", length=255)
+     */
+    private $alt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set src
+     *
+     * @param string $src
+     *
+     * @return Picture
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+
+        return $this;
+    }
+
+    /**
+     * Get src
+     *
+     * @return string
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+
+    /**
+     * Set alt
+     *
+     * @param string $alt
+     *
+     * @return Picture
+     */
+    public function setAlt($alt)
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    /**
+     * Get alt
+     *
+     * @return string
+     */
+    public function getAlt()
+    {
+        return $this->alt;
+    }
+}
+
